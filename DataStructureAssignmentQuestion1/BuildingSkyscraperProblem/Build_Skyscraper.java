2fd87fd0b69e01e85e4643290c9da494a5efe10b
HarikaChalla03 <107341665+HarikaChalla03@users.noreply.github.com>
2022-08-01 02:31:35 +0530

    Update Build_Skyscraper.java

--- DataStructureAssignmentQuestion1/BuildingSkyscraperProblem/Build_Skyscraper.java
+++ DataStructureAssignmentQuestion1/BuildingSkyscraperProblem/Build_Skyscraper.java
@@ -1,53 +1,9 @@
 package com.greatlearing.BuildingSkyscraperProblem ;
-
-import java.util.Scanner ;
 import java.util.Stack ;
 
 class Build_Skyscraper{
-	
-	public static void main( String[] args ) {
-	  try ( Scanner sc = new Scanner( System.in )) {
-		  System.out.print( " Enter the total number of floors in the building " ) ;
-			int number = sc.nextInt() ;
-		    @SuppressWarnings( "unused" )
-			int[] array = new int[number] ;
-            Stack < Integer >  stack  =  new  Stack< Integer >();
-			System.out.println( " Enter the size of floors given on day1: " );
-			int day1 = sc.nextInt() ; 
-			stack.push( day1 ) ;
-			System.out.println( " Enter the size of floors given on day2: " ) ;
-			int day2 = sc.nextInt() ; 
-			stack.push( day2 ) ;
-			System.out.println( " Enter the size of floors given on day3: " );
-			int day3 = sc.nextInt() ; 
-			stack.push( day3 ) ;
-			System.out.println( " Enter the size of floors given on day4: " ) ;
-			int day4 = sc.nextInt() ; 
-			stack.push( day4 ) ;
-			System.out.println( " Enter the size of floors given on day5: " ) ;
-			int day5 = sc.nextInt() ; 
-			stack.push( day5 ) ;
-			        
-            System.out.println( " The order of construction is as follows " ) ;
-                
-            if( stack.peek() < day1 ) 
-            stack.pop() ;
-            stack.pop() ;
-            stack.pop() ;
-            System.out.println( " day1 : ") ;
-            System.out.println( " day2 : ") ;
-            System.out.println( sortstack(stack) ) ;
-            System.out.println( " day3 : ");
-            System.out.println( " day4 : ") ;
-            System.out.println( " day5 : ") ;
-            stack.push( day3 ) ;
-            stack.push( day4 ) ;
-            stack.push( day5 ) ;
-            System.out.println( sortstack(stack) ) ;
-            }
-	    }
-	
-    public static   Stack< Integer >  sortstack( Stack< Integer > stack ){
+	public static   Stack< Integer >  sortstack( Stack< Integer > stack )
+	{
           Stack< Integer >  tmpStack  =  new  Stack< Integer >() ;
           while( !stack.isEmpty() )
           {
@@ -68,6 +24,7 @@ class Build_Skyscraper{
           return tmpStack ;
        }
 }
+
 
 
 
